acf961916e2c0387aae600fbb4a65779ecb52e7a
Quang Vo <vhnhatquang7399@gmail.com>
2020-03-23 14:44:49 +0700

    Second commit

--- src/main.java
+++ src/main.java
@@ -3,9 +3,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] array = new int[10];
+		int[] array = new int[20];
 		int count = 1;
-		for(int i=0; i<10; i++) {
+		for(int i=0; i<20; i++) {
 			array[i] = count;
 			count++;
 		}
